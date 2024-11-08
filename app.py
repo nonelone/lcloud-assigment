d9394ec88f30cb622c02ac6ae59586717c7c13bc
sha <nonelpublic@proton.me>
2024-11-08 17:15:28 +0100

    list_files now returns only the filename, not the whole path

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import boto3
+import re
 
 import secrets
 
@@ -10,22 +11,47 @@ s3 = boto3.resource(
 
 
 def list_files(bucket, prefix=""):
+    files = []
     for obj in bucket.objects.filter(Prefix=prefix):
-        if obj.key != ( prefix + "/" ): # do not print the root directory
-            print(obj.key)
+        file_name = obj.key.split("/")[-1]
+        if obj.key != ( prefix + "/" ) and file_name != "": # do not print the root directory
+            print(file_name)
+            files.append(file_name)
+
+    return files
+
+
 
 def upload_file(file, target):
     pass
 
-def filter_files(filter, bucket="*"):
-    pass
+def filter_files(re_filter, bucket="*", prefix=""):
+    files = []
+    for obj in bucket.objects.filter(Prefix=prefix):
+        if obj.key != ( prefix + "/" ): # ignore the root directory
+            files.append(obj.key)
 
-def delete_files_by_filter(filter, bucket="*"):
+    for file in files:
+        match = re.search(re_filter, file)
+        if not match:
+            files.remove(file)
+        else:
+            print(file)
+
+    return files
+
+    
+
+def delete_files_by_filter(re_filter, bucket="*", prefix=""):
     pass
 
 if __name__ == "__main__":
-    
+
     bucket = s3.Bucket('developer-task')
     prefix = "a-wing"
     
-    list_files(bucket, prefix)
+    _ = list_files(bucket, prefix)
+
+    example_filter = "^i"
+
+    _ = filter_files(example_filter, bucket, prefix)
